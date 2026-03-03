d5d387bd602e4aeafc1aeafbac827a73eec7a57a
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 22:43:33 +0300

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,1 +1,9 @@
+from aiogram import types
 
+def register_handlers(dp):
+    @dp.message_handler(commands=['menu'])
+    async def menu_cmd(message: types.Message):
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.add(types.InlineKeyboardButton("⚙️ اعدادات الحماية", callback_data="settings"))
+        keyboard.add(types.InlineKeyboardButton("👮‍♂️ الرتب", callback_data="ranks"))
+        await message.reply("اختر من القائمة:", reply_markup=keyboard)
