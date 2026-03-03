222d8fe0872327e5d7515de7cc76c6a41dca2e24
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 22:44:58 +0300

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,1 +1,14 @@
+import json
 
+DATA_FILE = "data.json"
+
+def load_data():
+    try:
+        with open(DATA_FILE, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+def save_data(data):
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
