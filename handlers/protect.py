8d10634e9a2942d4db4301436fc3e840edb5cd70
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 22:44:02 +0300

    Update protect.py

--- handlers/protect.py
+++ handlers/protect.py
@@ -1,1 +1,14 @@
+from aiogram import types
+import json
 
+def register_handlers(dp):
+    @dp.callback_query_handler(lambda c: c.data == 'settings')
+    async def protect_settings(callback_query: types.CallbackQuery):
+        await callback_query.message.answer("""
+🔒 اوامر الحماية:
+
+/قفل الروابط
+/فتح الروابط
+/قفل الصور
+/فتح الصور
+""")
