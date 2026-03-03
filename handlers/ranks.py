a74128e77134461f5dacb900fdbcd4d9503ef3aa
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 22:44:24 +0300

    Update ranks.py

--- handlers/ranks.py
+++ handlers/ranks.py
@@ -1,1 +1,13 @@
+from aiogram import types
 
+def register_handlers(dp):
+    @dp.callback_query_handler(lambda c: c.data == 'ranks')
+    async def show_ranks(callback_query: types.CallbackQuery):
+        await callback_query.message.answer("""
+👑 الرتب:
+
+• مطور
+• مالك
+• ادمن
+• عضو
+""")
