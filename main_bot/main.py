d994300d51caddcccbadae4d4e991d05828e6587
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-04 03:18:18 +0300

    Update main.py

--- main_bot/main.py
+++ main_bot/main.py
@@ -1,1 +1,13 @@
+from pyrogram import Client
+import config
+from plugins import general, admin, protection
 
+bot = Client("MainBot", bot_token=config.TOKEN)
+
+# تسجيل كل الملفات
+general.register(bot)
+admin.register(bot)
+protection.register(bot)
+
+print("🔥 البوت الأساسي بدأ العمل الآن!")
+bot.run()
