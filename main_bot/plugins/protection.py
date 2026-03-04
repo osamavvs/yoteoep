bf367a3ac35fd5580c138401e70dcb1a1c2d7dfc
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-04 03:19:45 +0300

    Update protection.py

--- main_bot/plugins/protection.py
+++ main_bot/plugins/protection.py
@@ -1,1 +1,9 @@
+from pyrogram import filters
 
+links_locked = {}
+
+def register(bot):
+    @bot.on_message(filters.text)
+    async def check_links(client, message):
+        if links_locked.get(message.chat.id) and "http" in message.text:
+            await message.delete()
