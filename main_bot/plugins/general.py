c10f48c76d6d794b67423c7c286018b343115173
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-04 03:18:50 +0300

    Update general.py

--- main_bot/plugins/general.py
+++ main_bot/plugins/general.py
@@ -1,1 +1,17 @@
+from pyrogram import filters
 
+def register(bot):
+    @bot.on_message(filters.text)
+    async def general_commands(client, message):
+        if message.text == "ابدأ":
+            await message.reply("🌙 مرحبا بك في بوت الحماية الاحترافي!")
+
+        elif message.text == "مساعدة":
+            await message.reply("""
+📌 أوامر البوت:
+
+- كتم / الغاء كتم
+- حظر / الغاء حظر
+- قفل روابط / فتح روابط
+- عرض الحالة
+            """)
